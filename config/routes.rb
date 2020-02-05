9d2a8ad451888410d797c216b4a2ad4ca95c1007
wsoccorsi <wsoccorsi@gmail.com>
2020-02-05 10:41:04 -0500

    root changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'welcome/home', to: 'welcome#home'
+  root 'welcome#home'
 
-  get 'welcome/about', to: 'welcome#about'
+  get 'about', to: 'welcome#about' #about path??
+
+
 end
